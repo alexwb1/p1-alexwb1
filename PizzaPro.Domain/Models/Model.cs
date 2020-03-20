43fdb8a48c0fc4bd0cbcd4b72086374e262d0dee
Alexander Barnard <alexwb1@live.com>
2020-03-19 21:23:15 -0500

    initial commit

---
+++ PizzaPro.Domain/Models/Model.cs
@@ -0,0 +1,10 @@
+
+namespace PizzaPro.Domain.Models
+{
+  public class Model
+  {
+    // private static readonly PizzaProDbContext _db = new PizzaProDbContext();
+    public string uid { get; set; }
+
+  }
+}
